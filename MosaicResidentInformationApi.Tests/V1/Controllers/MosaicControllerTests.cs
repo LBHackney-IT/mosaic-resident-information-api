cdee9b7d58cdfbb239398f0974ac25923e299016
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2020-05-18 12:34:22 +0100

    fix test

--- MosaicResidentInformationApi.Tests/V1/Controllers/MosaicControllerTests.cs
+++ MosaicResidentInformationApi.Tests/V1/Controllers/MosaicControllerTests.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using MosaicResidentInformationApi.V1.Controllers;
+using MosaicResidentInformationApi.V1.UseCase;
 using NUnit.Framework;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,12 +16,13 @@ namespace MosaicResidentInformationApi.Tests.V1.Controllers
     public class MosaicControllerTests
     {
         private MosaicController _classUnderTest;
-
+        private Mock<GetAllResidentsUseCase> _mock;
 
         [SetUp]
         public void SetUp()
         {
-            _classUnderTest = new MosaicController();
+            _mock = new Mock<GetAllResidentsUseCase>();
+            _classUnderTest = new MosaicController(_mock.Object);
         }
 
         [Test]
