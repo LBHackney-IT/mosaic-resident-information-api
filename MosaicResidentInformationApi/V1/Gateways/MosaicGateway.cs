4b045b75e9ebdd64542b0ab33c2fbaef233dc573
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2020-05-20 17:57:46 +0100

    rebased on master + fix e2e test build errors

--- MosaicResidentInformationApi/V1/Gateways/MosaicGateway.cs
+++ MosaicResidentInformationApi/V1/Gateways/MosaicGateway.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
+using MosaicResidentInformationApi.V1.Boundary.Requests;
+using MosaicResidentInformationApi.V1.Boundary.Responses;
 using MosaicResidentInformationApi.V1.Domain;
 using MosaicResidentInformationApi.V1.Factories;
 using MosaicResidentInformationApi.V1.Infrastructure;
