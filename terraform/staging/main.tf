a523b863a52dadfacdbab1f55cd32da33b05f5a3
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-08-29 11:34:18 +0100

    Remove the staging DMS related IAM roles.

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -104,23 +104,6 @@ data "aws_ssm_parameter" "mosaic_test_hostname" {
    }
  }
 
- resource "aws_iam_role" "dms-access-for-endpoint" {
-   assume_role_policy = "${data.aws_iam_policy_document.dms_assume_role.json}"
-   name               = "dms-access-for-endpoint"
- }
- resource "aws_iam_role_policy_attachment" "dms-access-for-endpoint-AmazonDMSRedshiftS3Role" {
-   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSRedshiftS3Role"
-   role       = "${aws_iam_role.dms-access-for-endpoint.name}"
- }
- resource "aws_iam_role" "dms-vpc-role" {
-   assume_role_policy = "${data.aws_iam_policy_document.dms_assume_role.json}"
-   name               = "dms-vpc-role"
- }
- resource "aws_iam_role_policy_attachment" "dms-vpc-role-AmazonDMSVPCManagementRole" {
-   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSVPCManagementRole"
-   role       = "${aws_iam_role.dms-vpc-role.name}"
- }
-
 # /*   ADD SECURITY RULE TO POSTGRES DB TO ALLOW DMS TRAFFIC   */
 data "aws_security_group" "mosaic_postgres_sg" {
    filter {
